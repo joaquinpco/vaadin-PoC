6d491fd046e3f04b16072b0a2e986b50a1f9f757
José Joaquín Pérez-Calderón Ortiz <joaquinpco@wlanipr36109.uca.es>
2019-11-07 19:57:32 +0100

    Using template

--- white-pearl/src/main/java/com/vaadin/Views/MainView.java
+++ white-pearl/src/main/java/com/vaadin/Views/MainView.java
@@ -1,24 +1,35 @@
 package com.vaadin.Views;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.router.Route;
 
 
 @Route("")
-public class MainView extends VerticalLayout {
+public class MainView extends AppLayout {
 
     public MainView() {
-    	
-    	//Cabecera
-    	add(new HeaderView());
-       
-     
-    	//Pie de página
-    	add(new FooterView());
+    	Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
+        img.setHeight("44px");
+        addToNavbar(new DrawerToggle(), img);
+        Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        addToDrawer(tabs);
+        
+        LoginForm component = new LoginForm();
+        /*component.addLoginListener(e -> {
+            boolean isAuthenticated = authenticate(e);
+            if (isAuthenticated) {
+                navigateToMainPage();
+            } else {
+                component.setError(true);
+            }
+        });*/
+        this.setContent(component);
     }
 
 }
