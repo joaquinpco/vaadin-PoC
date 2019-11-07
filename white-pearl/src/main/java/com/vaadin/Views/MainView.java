afb2551197c8a01d4b48cae078338feb5de6f50e
José Joaquín Pérez-Calderón Ortiz <joaquinpco@wlanipr36109.uca.es>
2019-11-07 18:45:51 +0100

    Header Footer y main

--- white-pearl/src/main/java/com/vaadin/Views/MainView.java
+++ white-pearl/src/main/java/com/vaadin/Views/MainView.java
@@ -7,13 +7,18 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 
+
 @Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
     	
+    	//Cabecera
+    	add(new HeaderView());
        
-       
+     
+    	//Pie de página
+    	add(new FooterView());
     }
 
 }
