b09980fb781848eb33beef0d7995715c187fed75
José Joaquín Pérez-Calderón Ortiz <joaquinpco@wlanipr36109.uca.es>
2019-11-14 19:03:26 +0100

    Servicio de Usuario

---
+++ wp/src/main/java/Services/UserService.java
@@ -0,0 +1,26 @@
+package Services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService{
+
+	private PasswordEncoder _pwdEncoder;
+	
+	public UserService(PasswordEncoder pwdEncoder)
+	{
+		_pwdEncoder = pwdEncoder;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) 
+			throws UsernameNotFoundException {
+		
+		return null;
+	}
+
+}
