bae1aa6e5e20b43b84cc1caf260590bbeb5824cc
José Joaquín Pérez-Calderón Ortiz <joaquinpco@wlanipr36109.uca.es>
2019-11-18 11:50:20 +0100

    Modulos Usuario

--- wp/src/main/java/Repository/UserRepository.java
+++ wp/src/main/java/Repository/UserRepository.java
@@ -1,5 +1,10 @@
 package Repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import Entity.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+
+	
 }
