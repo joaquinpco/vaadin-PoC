351a3f4a8089bf32dd549e80ac8f3fae374dc06d
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2019-11-21 19:47:00 +0100

    Vista Escala

--- wp/src/main/java/es/uca/iw/wp/spring/ScaleView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/ScaleView.java
@@ -2,21 +2,25 @@ package es.uca.iw.wp.spring;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 
 
-
+@Route("/verEscalas")
 public class ScaleView extends VerticalLayout {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1820114367662698117L;
-	
 	private Button _btnCheck;
 	private DatePicker _dBegin, _dEnd;
-	
+	private TextField _oTxtNextScale;
 	public ScaleView() {
+		H1 _hCabecera = new H1("Siguiente escala");
+		H2 _hContenido = new H2("a"/*Conexión con la base de datos*/);
 		_dBegin = new DatePicker();
 		_dBegin.setLabel("Fecha de llegada");
 		_dBegin.setPlaceholder("Fecha dentro de la longitud del crucero");
@@ -27,11 +31,10 @@ public class ScaleView extends VerticalLayout {
 		
 		_btnCheck = new Button("Comprobar escalas");
 		
-		HorizontalLayout chooseDates = new HorizontalLayout(_dBegin, _dEnd, _btnCheck);
-		_btnCheck.addClickListener(e->checkScale());
-	}
-	private Object checkScale() {
-		// TODO Auto-generated method stub
-		return null;
+		FormLayout chooseDates = new FormLayout();
+		chooseDates.add(_dBegin, _dEnd, _btnCheck);
+		add(_hCabecera, _hContenido, chooseDates);
+		_btnCheck.addClickListener(e->Notification.show("Prueba en la parte superior izquierda", 3000, Position.TOP_START)); //Acceso a la base de datos para pillar las escalas entre f1 y f2
 	}
 }
+
