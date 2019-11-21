3f5eee89fb8f057dbc058543cbfef67e5e37877d
Jose Manuel Morales Garcia <tricokh@gmail.com>
2019-11-21 19:44:20 +0100

    Update Restaurant.java

--- wp/src/main/java/es/uca/iw/wp/Entity/Restaurant.java
+++ wp/src/main/java/es/uca/iw/wp/Entity/Restaurant.java
@@ -3,8 +3,14 @@ package es.uca.iw.wp.Entity;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
 public class Restaurant {
-	
+	@Id
+	@GeneratedValue
+	private Long id;
 	private String _sName;
 	private Date _dtOpen, _dtClose;
 	//private List
@@ -23,7 +29,18 @@ public class Restaurant {
 		public Food(String sName, float fPrice)
 		{
 			_sName = sName;
-			_fPrice = fPrice;
+			setPrice(fPrice);
 		}
+		
+		public String getName() { return _sName;}
+		public void setName(String name) {_sName = name;}
+		public Date getOpen() {return _dtOpen;}
+		public void setOpen(Date timeOpen) {_dtOpen = timeOpen;}
+		public Date getClose() {return _dtClose;}
+		public void setClose(Date timeClose) {_dtClose = timeClose;}
+		public void addFood(Food plate) { _lstFood.add(plate);}
+		public void addFood(String name, float price) { Food plate = new Food(name , price); _lstFood.add(plate);}
+		public float getPrice() {return _fPrice;}
+		public void setPrice(float price) {_fPrice = price;}
 	}
 }
