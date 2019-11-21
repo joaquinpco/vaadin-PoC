95e8875865c47fcf4d7111e28c25aee5e0e7f47a
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2019-11-21 19:46:22 +0100

    Repositorio Escala
    Vacio, de momento

---
+++ wp/src/main/java/es/uca/iw/wp/Repository/ScaleRepository.java
@@ -0,0 +1,9 @@
+package es.uca.iw.wp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.uca.iw.wp.Entity.Scale;
+
+public interface ScaleRepository extends JpaRepository<Scale, Integer>{
+	
+}
