6175ff0320fa5ff26d0f4ac438ace5f0fa184506
José Joaquín Pérez-Calderón Ortiz <joaquinpco@wlanipr36109.uca.es>
2019-11-28 18:09:07 +0100

    Login User works

--- wp/src/main/java/es/uca/iw/wp/Application.java
+++ wp/src/main/java/es/uca/iw/wp/Application.java
@@ -1,10 +1,17 @@
 package es.uca.iw.wp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import es.uca.iw.wp.Entity.User;
+import es.uca.iw.wp.Repository.UserRepository;
 
 /**
  * The entry point of the Spring Boot application.
@@ -13,8 +20,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public class Application extends SpringBootServletInitializer {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+	public CommandLineRunner loadData(UserRepository repository) {
+		return (args) -> {
+				repository.save(new User("Pablo", "Piedad", passwordEncoder.encode("1234"), 1111));
+		};
+    }
 }
