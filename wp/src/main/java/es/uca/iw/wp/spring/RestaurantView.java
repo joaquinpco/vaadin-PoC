2d4ef2fcc7c33bcb9b7c533c21aeffa011643258
Jose Manuel Morales Garcia <tricokh@gmail.com>
2019-12-12 17:10:15 +0100

    actualizacion de restaurant y view primer prototipo

--- wp/src/main/java/es/uca/iw/wp/spring/RestaurantView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/RestaurantView.java
@@ -2,7 +2,10 @@ package es.uca.iw.wp.spring;
 
 import com.vaadin.flow.component.accordion.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 
+@Route("showRestaurants")
 public class RestaurantView extends VerticalLayout {
 	/**
 	 * 
@@ -13,31 +16,54 @@ public class RestaurantView extends VerticalLayout {
 	 * UI/UX atributes
 	 */
 	private Accordion _oAcrdnRestaurants;
-	private Accordion _oAcrdnHorario;
-	private Accordion _oAcrdntables;
-	private Accordion _oAcrdnMenu;
+	private Accordion _oAcrdnList;
+
+	private VerticalLayout _oVrtlHorario;
+	private VerticalLayout _oVrtltables;
+	private VerticalLayout _oVrtlMenu;
 	private VerticalLayout _oVrtlNewRestaurant;
-	private VerticalLayout _oVrtlAuxiliar;
+
 	
 	private void initUIView()
 	{
 		//Vertical Layout
 		_oVrtlNewRestaurant = new VerticalLayout();
-		_oVrtlAuxiliar = new VerticalLayout();
-		
+		_oVrtlMenu = new VerticalLayout();
+		_oVrtlHorario = new VerticalLayout();
+		_oVrtltables = new VerticalLayout();
 		//Accordions
-		_oAcrdnHorario = new Accordion();
-		_oAcrdntables = new Accordion();
-	    _oAcrdnMenu = new Accordion();
+		_oAcrdnList = new Accordion();
 	    _oAcrdnRestaurants = new Accordion();
+	    
 	  /*Se lo carga todo
 	  		auxiliarVL.removeAll();*/
 	}
-	//auxiliarVL.removeAll();
 	public RestaurantView()
 	{
 		//Intialize UI/UX
-		initUIView();	
+		initUIView();
+		
+		_oVrtlHorario.add(
+				new TextField("Hora de apertura: "),
+				new TextField("Hora de cierre: ")
+		);
+		_oAcrdnRestaurants.add("Horario", _oVrtlHorario);
+		
+		_oVrtltables.add(
+				new TextField("Numero de mesas: ")
+		);
+		_oAcrdnRestaurants.add("Mesas", _oVrtlHorario);
+		
+		//while(//mas elementos de la bbdd) {
+		_oVrtlMenu.add(
+					new TextField("Nombre Plato, Precio Plato €")//sustituir por el nombre del plato de la bbdd
+			);
+		//}
+		_oAcrdnRestaurants.add("Menú", _oVrtlMenu);
+		
+		_oVrtlNewRestaurant.add(_oAcrdnRestaurants);
+		_oAcrdnList.add("Nombre del Restaurante", _oVrtlNewRestaurant);
+		
 	}
 	/*
 	newRestaurant.add(//nombre del restaurante al añadirlo	
