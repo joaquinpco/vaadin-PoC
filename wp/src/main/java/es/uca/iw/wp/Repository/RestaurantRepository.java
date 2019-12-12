ece51dc62de3f90b3f11f58f2f2a89162ca03df0
Jose Manuel Morales Garcia <tricokh@gmail.com>
2019-12-12 20:02:31 +0100

    app layout

---
+++ wp/src/main/java/es/uca/iw/wp/Repository/RestaurantRepository.java
@@ -0,0 +1,17 @@
+package es.uca.iw.wp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+import es.uca.iw.wp.Entity.Restaurant;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>{
+	
+	@Query("select u from Restaurant u")
+	List<Restaurant> listRestaurant();
+	
+	@Query("select u from Restaurant u where u.id = :id ")
+	Restaurant findByIds(@Param("id") Integer id);
+}
