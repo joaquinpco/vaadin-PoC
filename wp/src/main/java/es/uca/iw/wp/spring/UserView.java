222903fc13b4b9b90ee751aa26593e6b6fd1f699
joaquinpco <your-correct-email@example.com>
2019-12-27 00:45:20 +0100

    Views Integrated

--- wp/src/main/java/es/uca/iw/wp/spring/UserView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/UserView.java
@@ -1,7 +1,26 @@
 package es.uca.iw.wp.spring;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class UserView extends VerticalLayout{
+import es.uca.iw.wp.Entity.User;
+import es.uca.iw.wp.Security.SecurityUtils;
 
+public class UserView extends VerticalLayout{
+	
+	private Button _oBtnMisGastos;
+	
+	public UserView()
+	{
+		User oUser = SecurityUtils.getSesionUser();
+		
+		H2 h2Name = new H2("Name: " + oUser.getName());
+		
+		H2 h2LastName = new H2("Last Name:" + oUser.getLastName());
+		
+		_oBtnMisGastos = new Button("My expenses");
+		
+		add(h2Name, h2LastName, _oBtnMisGastos);
+	}
 }
