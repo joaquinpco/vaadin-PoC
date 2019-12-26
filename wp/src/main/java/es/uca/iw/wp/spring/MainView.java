cb901679460020a65f021f4443ef21ab8d9f88e3
joaquinpco <your-correct-email@example.com>
2019-12-27 00:44:26 +0100

    Tabs menu integrated

--- wp/src/main/java/es/uca/iw/wp/spring/MainView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/MainView.java
@@ -14,6 +14,7 @@ import com.vaadin.flow.router.Route;
 
 import es.uca.iw.wp.Security.SecurityUtils;
 import es.uca.iw.wp.Services.RestaurantService;
+import es.uca.iw.wp.Services.ScaleService;
 
 @Route("")
 public class MainView extends AppLayout {
@@ -23,29 +24,33 @@ public class MainView extends AppLayout {
 	 */
 	private static final long serialVersionUID = 1796776387882508405L;
 
-	private RestaurantService restaurantService;
+	private RestaurantService _oRestaurantService;
+	
+	private ScaleService _oScaleService;
 		
 	@Autowired
-	public MainView(RestaurantService restaurantService) {
-		this.restaurantService=restaurantService;
+	public MainView(RestaurantService restaurantService, ScaleService scaleService) {
+		
+		_oRestaurantService = restaurantService;
+		_oScaleService = scaleService;
+		
 		Image img = new Image("frontend/img/WPlogo.png", "WPlogo");
         img.setHeight("44px");
+        
         Button btnLogOut = new Button("log Out");
         btnLogOut.getStyle().set("margin-left", "60%");
         addToNavbar(img);
         
         final boolean touchOptimized = true;
         addToNavbar(touchOptimized, new DrawerToggle(), img);
+        
         Tabs tabs = new Tabs(new Tab("Home"), new Tab("Scale"), 
         		new Tab("Book"), new Tab("Profile"));
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        
         addToDrawer(tabs);
         
         
         addToNavbar(new H3("Bienvenid@ " + SecurityUtils.getSesionUser()));
-        
-        
         addToNavbar(btnLogOut);
         
         btnLogOut.addClickListener(e->{
@@ -53,9 +58,21 @@ public class MainView extends AppLayout {
         	getUI().get().getPage().reload();
         });
         
+        tabs.addSelectedChangeListener(e->{
+        	
+        	switch(e.getSelectedTab().getElement().getText())
+            {
+            	case "Scale":
+            		setContent(new ScaleView(_oScaleService));
+            		break;
+            	case "Profile":
+            		setContent(new UserView());
+            		break;
+            }
+        });
         
+        //Set view for each tab item
         
-        //swichtabs.getElement().getText())
        
     }
 
