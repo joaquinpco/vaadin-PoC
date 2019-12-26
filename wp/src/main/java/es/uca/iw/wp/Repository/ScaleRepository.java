2909892fa2861fb83046b91929c3f92bd7679cd3
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2019-12-26 18:21:25 +0100

    Upgrade and upload

--- wp/src/main/java/es/uca/iw/wp/Repository/ScaleRepository.java
+++ wp/src/main/java/es/uca/iw/wp/Repository/ScaleRepository.java
@@ -2,15 +2,25 @@ package es.uca.iw.wp.Repository;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import es.uca.iw.wp.Entity.Restaurant;
 import es.uca.iw.wp.Entity.Scale;
 
 public interface ScaleRepository extends JpaRepository<Scale, Integer>{
-	Scale findById(int _iScaleId);
 	
-	List<Scale> findAllBy_dTimeNowBetween(LocalDate begin, LocalDate end);
+	@Query("select u from Scale u where u._iScaleId = :id")
+	Scale findByIds(@Param("id") Integer id);
+	
+	/*@Query("select u from Scale u where u._dTimeNow is between u._dArrive= :dArrive and u._dDeparture= :dDeparture")
+	List<Scale> findAllBy_dTimeNow(@Param("dArrive, dDeparture") LocalDate dArrive, LocalDate dDeparture);
+	*/
+	@Query("select u from Scale u where u._dTimeNow = :now")
+	Scale findBy_dTimeNow(@Param("now") LocalDate now);
 }
