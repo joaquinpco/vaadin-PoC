7c2b9f70aa75e7f40a5c150a86a00518d64e75cb
joaquinpco <joaquin1208@me.com>
2019-12-30 19:43:18 +0100

    Scale view

--- wp/src/main/java/es/uca/iw/wp/spring/ScaleView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/ScaleView.java
@@ -19,6 +19,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import es.uca.iw.wp.Entity.Scale;
+import es.uca.iw.wp.Repository.ScaleRepository;
 import es.uca.iw.wp.Services.ScaleService;
 
 
@@ -32,32 +33,44 @@ public class ScaleView extends VerticalLayout {
 	private Button _btnCheck;
 	private Button _btnGoToCity;
 	private DatePicker _dBegin, _dEnd;
+	private H1 _oTxtInfo;
 	
 	public ScaleView(ScaleService _oScService) {
 		this._oScService = _oScService;
-		Scale s = _oScService.findById(0);
 		
-		H1 _hCabecera = new H1("Escala: " + s.getPort());
-		_btnGoToCity = new Button("Ver información de " + s.getPort());
-		_btnGoToCity.addClickListener(e->{
-			_btnGoToCity.getUI().ifPresent(ui->ui.navigate("showCity"));//Con esto, se hace como un href a showCity
+		long iCount  =  _oScService.numberOfScales();
+		
+		if(iCount > 0)
+		{
+			Scale s = _oScService.findById(0);
+			
+			H1 _hCabecera = new H1("Escala: " + s.getPort());
+			_btnGoToCity = new Button("Ver información de " + s.getPort());
+			_btnGoToCity.addClickListener(e->{
+				_btnGoToCity.getUI().ifPresent(ui->ui.navigate("showCity"));//Con esto, se hace como un href a showCity
+			}
+			);
+			
+			H2 _hContenido = new H2("Siguiente escala -> ");
+			_dBegin = new DatePicker();
+			_dBegin.setLabel("Fecha de llegada");
+			_dBegin.setPlaceholder("Fecha dentro de la longitud del crucero");
+			
+			_dEnd = new DatePicker();
+			_dEnd.setLabel("Fecha de salida");
+			_dEnd.setPlaceholder("Fecha dentro de la longitud del crucero");
+			
+			_btnCheck = new Button("Comprobar escalas");
+			
+			FormLayout chooseDates = new FormLayout();
+			chooseDates.add(_dBegin, _dEnd, _btnCheck);
+			add(_hCabecera, _btnGoToCity, _hContenido, chooseDates);
 		}
-		);
-		
-		H2 _hContenido = new H2("Siguiente escala -> ");
-		_dBegin = new DatePicker();
-		_dBegin.setLabel("Fecha de llegada");
-		_dBegin.setPlaceholder("Fecha dentro de la longitud del crucero");
-		
-		_dEnd = new DatePicker();
-		_dEnd.setLabel("Fecha de salida");
-		_dEnd.setPlaceholder("Fecha dentro de la longitud del crucero");
-		
-		_btnCheck = new Button("Comprobar escalas");
-		
-		FormLayout chooseDates = new FormLayout();
-		chooseDates.add(_dBegin, _dEnd, _btnCheck);
-		add(_hCabecera, _btnGoToCity, _hContenido, chooseDates);
+		else
+		{
+			_oTxtInfo = new H1("No hay información sobre escalas actualmente");
+			add(_oTxtInfo);
+		}
 		//_btnCheck.addClickListener(e->obtainTable(_dBegin.getValue(), _dEnd.getValue())); //Acceso a la base de datos para pillar las escalas entre f1 y f2
 	}
 	/*private void obtainTable(LocalDate dArrive, LocalDate dDeparture) {
