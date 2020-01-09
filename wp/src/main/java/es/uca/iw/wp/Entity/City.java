cf77616a46ae065123fa0b99fe7c2471e5aa23d0
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2020-01-09 18:47:41 +0100

    Upload of Entities

--- wp/src/main/java/es/uca/iw/wp/Entity/City.java
+++ wp/src/main/java/es/uca/iw/wp/Entity/City.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -16,16 +17,28 @@ public class City {
 	
 	private String _sName;
 	private String _sDescrip;
-	@OneToMany
+	private double _dLon;
+	private double _dLat;
+	public String _sImagePath;
+	public String _sUrlVideo;
+	@OneToMany(fetch=FetchType.EAGER)
 	private List<Excursion> _lstExcursion = new ArrayList<Excursion>();
+	
 	public City() {}
-	public City(String sName, String sDescrip) {
+	public City(String sName, String sDescrip, double dLon, double dLat) {
 		_sName = sName;
 		_sDescrip = sDescrip;
+		_dLon = dLon;
+		_dLat = dLat;
 	}
 	
 	public String getName() {return _sName;}
 	public String getDescrip() {return _sDescrip;}
+	public double getLon() {return _dLon;}
+	public double getLat() {return _dLat;}
+	public String getImagePath() {return _sImagePath;}
+	public String getUrlVideo() {return _sUrlVideo;}
+	public List<Excursion> getExcursions(){return _lstExcursion;}
 	
 	public void setCityId(int id) {_iId = id;}
 	public void setName(String name) {
@@ -35,4 +48,5 @@ public class City {
 	public void setDescrip(String descrip) {
 		_sDescrip = descrip;
 	}
+	
 }
