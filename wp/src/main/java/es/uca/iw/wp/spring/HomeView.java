c7af16bd3bafd397d9a86da2f45007597c88fdde
joaquinpco <joaquin1208@me.com>
2020-01-13 22:36:27 +0100

    Create HomeView.java

---
+++ wp/src/main/java/es/uca/iw/wp/spring/HomeView.java
@@ -0,0 +1,17 @@
+package es.uca.iw.wp.spring;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class HomeView extends VerticalLayout
+{
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1176188131491314121L;
+
+public HomeView()
+ {
+	 //this.setSizeFull();
+	 this.addClassName("homeScreen");
+ }
+}
