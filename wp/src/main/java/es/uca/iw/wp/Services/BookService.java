0d6dc9a2fcd21f81e4a88964310306f52ac55c90
joaquinpco <joaquin1208@me.com>
2020-01-16 00:36:12 +0100

    User Can Watch books

---
+++ wp/src/main/java/es/uca/iw/wp/Services/BookService.java
@@ -0,0 +1,8 @@
+package es.uca.iw.wp.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+	
+}
--- wp/src/main/java/es/uca/iw/wp/spring/UserView.java
+++ wp/src/main/java/es/uca/iw/wp/spring/UserView.java
@@ -1,11 +1,15 @@
 package es.uca.iw.wp.spring;
 
+import java.util.List;
+
 import org.springframework.security.access.annotation.Secured;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import es.uca.iw.wp.Entity.Book;
 import es.uca.iw.wp.Entity.User;
 import es.uca.iw.wp.Repository.BookRepository;
 import es.uca.iw.wp.Security.SecurityUtils;
@@ -18,9 +22,12 @@ public class UserView extends VerticalLayout{
 	 */
 	private static final long serialVersionUID = 5073527090749853594L;
 	private Button _oBtnMyBooks;
+	private BookRepository _oBookRepository;
+	private Grid<Book> oGrid;
 	
-	public UserView()
+	public UserView(BookRepository oBookRepository)
 	{
+		_oBookRepository = oBookRepository;
 		
 		User oUser = SecurityUtils.getSesionUser();
 		
@@ -37,6 +44,28 @@ public class UserView extends VerticalLayout{
 		_oBtnMyBooks.addClickListener(e->{
 			
 			oCurrentLayout.removeAll();
+			
+			oGrid = new Grid<>();
+			
+			oGrid.removeAllColumns();
+			
+			if( _oBookRepository.count() > 0)
+			{
+				List<Book> oLstBooks = _oBookRepository.findByUsers(oUser);
+				
+				//Only showed if there is at least one.
+				if(oLstBooks.size() > 0)
+				{
+				
+					oGrid.setItems(oLstBooks);
+					oGrid.addColumn(Book::getFechaReserva).setHeader("Fecha Reserva");
+					oGrid.addColumn(Book::getAforoPosicionUsr).setHeader("Aforo");
+					oGrid.addColumn(Book::getTipo).setHeader("Tipo");
+					oCurrentLayout.add(oGrid);
+					
+				}
+				
+			}	
 			
 		});
 	}
